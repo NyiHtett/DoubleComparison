a3d1c972b9b46829c1e05df485f99840c56ec052
Nyi Htet <nyihtet4dev@icloud.com>
2023-02-14 18:02:27 -0800

    first commit

--- DoubleComparison/main.cpp
+++ DoubleComparison/main.cpp
@@ -1,14 +1,20 @@
-//
-//  main.cpp
-//  DoubleComparison
-//
-//  Created by Nyi Htet on 2/14/23.
-//
-
-#include <iostream>
-
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+#include<iostream>
+#include<math.h>
+#include<iomanip>
+using namespace std;
+int main(){
+    //sqrt(2) times sqrt(2) = 2;
+    double x = sqrt(2);
+    double y = 2.0;
+    double EPSILON = 1e-14;
+    if (x*x == y){
+        cout << "sqrt(2) * sqrt(2) is 2.0" << endl;
+    }
+    else{
+        cout << "sqrt(2) * sqrt(2) is not 2.0 but " << fixed << setprecision(18) << x*x << endl;
+    }
+    if (abs(x*x - y) < EPSILON){
+        cout << "approximate 2" << endl;
+    }
     return 0;
 }
